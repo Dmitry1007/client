474ff7d5010023a2df018c10c44db69f345f208c
Dmitry Sky <dmitry.sky@icloud.com>
2020-09-09 16:24:09 -0700

    Scaffold out all of the CRUD components and routes for handling streams

--- src/components/App.js
+++ src/components/App.js
@@ -1,30 +1,21 @@
 import React from 'react';
-import { BrowserRouter, Route, Link } from 'react-router-dom';
+import { BrowserRouter, Route } from 'react-router-dom';
 
-const PageOne = () => {
-    return (
-        <div>
-            <h2>PageOne</h2>
-            <Link to="/pagetwo">Page Two</Link>
-        </div>
-    )
-}
-
-const PageTwo = () => {
-    return (
-        <div>
-            <h2>PageTwo</h2>
-            <Link to="/">Page One</Link>
-        </div>
-    )
-}
+import StreamList from './streams/StreamList';
+import StreamCreate from './streams/StreamCreate';
+import StreamShow from './streams/StreamShow';
+import StreamEdit from './streams/StreamEdit';
+import StreamDelete from './streams/StreamDelete';
 
 const App = () => {
     return (
         <div>
             <BrowserRouter>
-                <Route path="/" exact component={PageOne} />
-                <Route path="/pagetwo" component={PageTwo} />
+                <Route path="/" exact component={StreamList} />
+                <Route path="/streams/new" exact component={StreamCreate} />
+                <Route path="/streams/show" exact component={StreamShow} />
+                <Route path="/streams/edit" exact component={StreamEdit} />
+                <Route path="/streams/delete" exact component={StreamDelete} />
             </BrowserRouter>
         </div>
     )
