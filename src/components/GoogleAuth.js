c684939602522fe64f067929f0ebfdddb31eab1e
Dmitry Sky <dmitry.sky@icloud.com>
2020-09-12 10:35:10 -1000

    Dynamically update UI every time users auth changes

--- src/components/GoogleAuth.js
+++ src/components/GoogleAuth.js
@@ -5,15 +5,22 @@ class GoogleAuth extends React.Component {
 
     componentDidMount() {
         window.gapi.load('client:auth2', () => {
-            window.gapi.client.init({
+            window.gapi.client
+            .init({
                 clientId: process.env.REACT_APP_GOOGLE_CLIENT_ID,
                 scope: 'email'
-            }).then(() => {
+            })
+            .then(() => {
                 this.auth = window.gapi.auth2.getAuthInstance();
-                this.setState({ isSignedIn: this.auth.isSignedIn.get() })
+                this.setState({ isSignedIn: this.auth.isSignedIn.get() });
+                this.auth.isSignedIn.listen(this.onAuthChange);
             });
         });
     }
+
+    onAuthChange = () => {
+        this.setState({ isSignedIn: this.auth.isSignedIn.get() });
+    };
 
     renderAuthButton() {
         if (this.state.isSignedIn === null) {
