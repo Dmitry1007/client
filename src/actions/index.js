9a3b896c997f6c5290609d49c20be736c67dc9d5
Dmitry Sky <dmitry.sky@icloud.com>
2020-09-26 12:46:57 -1000

    Remove strict mode, add fetchStream action

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,5 +1,5 @@
 import streams from '../apis/streams';
-import { SIGN_IN, SIGN_OUT } from './types';
+import { SIGN_IN, SIGN_OUT, CREATE_STREAM } from './types';
 
 export const signIn = (userId) => {
     return {
@@ -15,5 +15,18 @@ export const signOut = () => {
 };
 
 export const createStream = formValues => async dispatch => {
-    streams.post('/streams', formValues);
+    const response = await streams.post('/streams', formValues);
+    dispatch({
+        type: CREATE_STREAM,
+        payload: response
+    });
+};
+
+export const fetchStream = formValues => async dispatch => {
+    const response = await streams.get('/streams');
+    console.log(response);
+    // dispatch({
+    //     type: FETCH_STREAM,
+    //     payload: response
+    // });
 };
