ce5031142ce4b336b18ef98652f34775d34ed32d
Dmitry Sky <dmitry.sky@icloud.com>
2020-10-20 12:55:59 -1000

    Prevent event bubbling in case user accidently clicks inside the modal but does not click a button

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -4,8 +4,14 @@ import history from '../history';
 
 const Modal = props => {
     return ReactDOM.createPortal(
-        <div onClick={() => history.push('/')} className="ui dimmer modals visible active">
-            <div className="ui standard modal visible active">
+        <div 
+            onClick={() => history.push('/')} 
+            className="ui dimmer modals visible active"
+        >
+            <div 
+                className="ui standard modal visible active"
+                onClick={(e) => e.stopPropagation()}
+            >
                 <div className="header">Delete Stream</div>
                 <div className="content">
                     Are you sure you want to delete the stream?
